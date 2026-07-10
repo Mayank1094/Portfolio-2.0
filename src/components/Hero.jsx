27878e90b58f4470996631a41eb06ad60e8f4c4c
Mayank1094 <mayankkamble1094@gmail.com>
2026-07-10 13:36:27 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -183,7 +183,7 @@ export default function Hero() {
             </button>
             <a
               id="hero-cta-resume"
-              href="/resume.pdf"
+              href="/Resume.pdf"
               className="btn-secondary"
               download
             >
